dc91937626c9178cd974d3b466a388bed88081e6
Guisielo <155173663+Guisielo@users.noreply.github.com>
2026-06-19 18:00:17 +0000

    Proyecto casi terminado

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planet, Vehicle, Species, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -18,7 +18,8 @@ db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
+    #app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 MIGRATE = Migrate(app, db)
@@ -45,7 +46,121 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+@app.route ('/character', methods = ['GET'])
+def get_all_characters ():
+   characters = Character.query.all()
+   return jsonify ([character.serialize() for character in classharacters]), 200
+
+@app.route ('/character/<int:character_id>', methods = ['GET'])
+def get_character (character_id):
+    character = Character.query.get (character_id)
+    if character is None:
+        raise APIException ("Character is not found", status_code = 404)
+    return jsonify (character.serialize ()), 200
+
+@app.route ('/planets', methods = ['GET'])
+def get_all_planets ():
+    planets = Planet.query.all ()
+    return jsonify ([planet.serialize () for planet in planets]), 200
+
+@app.route ('/planets/<int:planet_id>', methods = ['GET'])
+def get_planet (planet_id):
+    planet = Planet.query.get (planet_id)
+    if planet is None:
+        raise APIException ("Planet is not found", status_code = 404)
+    return jsonify (planet.serialize()), 200
+
+@app.route ('/users', methods = ['GET'])
+def get_all_users ():
+    users = User.query.all ()
+    return jsonify ([user.serialize () for user in users]), 200
+
+@app.route ('/users/<int:user_id>/favorites', methods = ['GET'])
+def get_all_favorites (user_id):
+    favorites = Favorite.query.filter_by (user_id = user_id).all ()
+    return jsonify ([favorite.serialize () for favorite in favorites]), 200
+
+@app.route ('/favorite/planet/<int:planet_id>', methods = ['POST'])
+def add_favorite_planet (planet_id):
+    planet = Planet.query.get (planet_id)
+
+    if planet is None:
+        raise APIException ('Planet not found', status_code = 404)
+
+    new_favorite = Favorite (user_id = 1, planet_id = planet_id)
+
+    try:
+        db.session.add (new_favorite)
+        db.session.commit ()
+    
+    except Exception as e:
+        raise APIException (f"Failed to add planet to favorites", status_code = 500) from e
+
+    return jsonify ({
+        "msg": "Planet was added succesfully",
+        "favorite": new_favorite.serialize ()
+    }), 201
+
+@app.route ('/favorite/character/<int:character_id>', methods = ['POST'])
+def add_favorite_character (character_id):
+    character = Character.query.get (character_id)
+
+    if character is None:
+        raise APIException ('Character not found', status_code = 404)
+
+    new_favorite = Favorite (user_id = 1, character_id = character_id)
+
+    try:
+        db.session.add (new_favorite)
+        db.session.commit ()
+    
+    except Exception as e:
+        raise APIException (f"Failed to add character to favorites", status_code = 500) from e
+
+    return jsonify ({
+        "msg": "Character was added succesfully",
+        "favorite": new_favorite.serialize ()
+    }), 201
+
+@app.route ('/favorite/planet/<int:planet_id>', methods = ['DELETE'])
+def delete_favorite_planet (planet_id):
+    searched_favorite_planet = Favorite.query.filter_by (planet_id = planet_id, user_id = 1).first()
+
+    if not searched_favorite_planet:
+        raise APIException (f"Favorite planet is not found", status_code = 404)
+    
+    try:
+        db.session.delete (searched_favorite_planet)
+        db.session.commit ()
+    
+    except Exception as e:
+        raise APIException (f"Something wrong happened", status_code = 500) from e
+
+    return jsonify({
+        "msg": "Favorite planet deleted"
+    }), 200
+
+@app.route ('/favorite/character/<int:character_id>', methods = ['DELETE'])
+def delete_favorite_character (character_id):
+    searched_favorite_character = Favorite.query.filter_by (character_id = character_id, user_id = 1).first()
+
+    if not searched_favorite_character:
+        raise APIException (f"Favorite character not found", status_code = 404)
+    
+    try:
+        db.session.delete (searched_favorite_character)
+        db.session.commit ()
+    
+    except Exception as e:
+        raise APIException (f"Something wrong happened", status_code = 500) from e
+
+    return jsonify({
+        "msg": "Favorite character deleted"
+    }), 200
+
+
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+    #app.run(host='0.0.0.0', port=PORT, debug=False)
+    app.run(host='0.0.0.0', port=PORT, debug=True)
--- src/models.py
+++ src/models.py
@@ -1,19 +1,141 @@
+from enum import Enum as pyEnum
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey, Enum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "user"
+
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
+    name: Mapped [str] = mapped_column (String (20), nullable = False)
+    surname: Mapped [str] = mapped_column (String (20), nullable = False)
+    username: Mapped [str] = mapped_column (String (10), unique = True, nullable = False)
+    email: Mapped[str] = mapped_column(String(20), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String (20), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
+            "surname": self.surname,
+            "username": self.username,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Planet (db.Model):
+    __tablename__ = "planet"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped [str] = mapped_column (String (20), nullable = False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+class Gender (pyEnum):
+    Female = 1
+    Male = 2
+
+class Species (db.Model):
+    __tablename__ = "species"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped [str] = mapped_column (String (20), nullable = False)
+    language: Mapped [str] = mapped_column (String(20), nullable = True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "language": self.language
+        }
+
+class Character (db.Model):
+    __tablename__ = "character"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped [str] = mapped_column (String (20), nullable = False)
+    description: Mapped [str] = mapped_column (String ())
+
+    planet_id: Mapped [int]= mapped_column (ForeignKey ("planet.id"))
+    planet: Mapped [Planet] = relationship ()
+
+    species_id: Mapped [int]= mapped_column (ForeignKey ("species.id"))
+    species: Mapped [Species] = relationship ()
+
+    gender: Mapped [Gender] = mapped_column (Enum (Gender))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "descripcion": self.description,
+            "planet": {
+                "id": self.planet.id,
+                "name": self.planet.name,
+            } if self.planet else None,
+            "species": {
+                "id": self.species.id,
+                "name": self.species.name
+            } if self.species else None,
+            "gender": self.gender.value
+        }
+
+class Vehicle (db.Model):
+    __tablename__ = "vehicle"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped [str] = mapped_column (String (20), nullable = False)
+    description: Mapped [str] = mapped_column (String (), nullable = True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description
+        }
+
+class Favorite (db.Model):
+    __tablename__ = "favorite"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    user_id: Mapped [int]= mapped_column (ForeignKey ("user.id"))
+    user: Mapped [User] = relationship ()
+
+    character_id: Mapped [int] = mapped_column (ForeignKey ("character.id"), nullable=True)
+    character: Mapped [Character] = relationship ()
+
+    planet_id: Mapped [int] = mapped_column (ForeignKey ("planet.id"), nullable=True)
+    planet: Mapped [Planet] = relationship ()
+
+    vehicle_id: Mapped [int] = mapped_column (ForeignKey ("vehicle.id"), nullable=True)
+    vehicle: Mapped [Vehicle] = relationship ()
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": {
+                "id":self.user.id,
+                "name": self.user.name,
+            } if self.user else None,
+            "character": {
+                "id": self.character.id,
+                "name": self.character.name
+            } if self.character else None,
+            "planet": {
+                "id": self.planet.id,
+                "name": self.planet.name
+            } if self.planet else None,
+            "vehicle": {
+                "id": self.vehicle.id,
+                "name": self.vehicle.name
+            } if self.vehicle else None,
         }
